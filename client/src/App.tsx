205eb22483f9450b9f4a7dea2d78c85ee810357a
hankyeol-jung <whd6023@gmail.com>
2023-03-26 19:36:37 +0900

    메뉴 네비게이션

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function App() {
-  return <div className="App bg-red-700">포트폴리오</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <nav className="bg-gray-300">
+      <div className="flex items-center justify-between px-10 mx-auto max-w-7xl h-14">
+        <div>한결</div>
+        <ul className="flex items-center text-sm font-medium text-white">
+          <li className="p-2 mx-2">포트폴리오</li>
+          <li className="p-2 mx-2">방명록</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default App;
