2967bf451c6f07ab9f8859fb59be39df73deec83
hankyeol-jung <whd6023@gmail.com>
2023-04-04 15:58:08 +0900

    프로젝트 반응형 뷰

--- client/src/Routes/Project.tsx
+++ client/src/Routes/Project.tsx
@@ -1,5 +1,24 @@
 import { useParams } from "react-router-dom";
 import { projectData } from ".././data/data";
+import React, { useState, useEffect } from "react";
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  FadeOut,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  StickyOut,
+  Zoom,
+  ZoomIn,
+  ZoomOut,
+} from "react-scroll-motion";
 
 function Project() {
   // projectId를 가져옵니다.
@@ -12,17 +31,148 @@ function Project() {
   const project =
     id !== undefined ? projectData.find((data) => data.id === id) : undefined;
 
+  let [imageLength, setImageLength] = useState<{ id: number; chk: boolean }[]>(
+    []
+  );
+
+  useEffect(() => {
+    if (project?.imageLength) {
+      let copy: { id: number; chk: boolean }[] = [];
+      for (let i = 0; i < project.imageLength; i++) {
+        copy.push({ id: i, chk: i === 0 });
+      }
+      setImageLength(copy);
+    }
+  }, []);
+
+  function handleChkChange(id: number, value: boolean) {
+    const newButtons = imageLength.map((a) => {
+      if (a.id === id) {
+        return { ...a, chk: value };
+      } else {
+        return { ...a, chk: false };
+      }
+    });
+    setImageLength(newButtons);
+  }
+
   return (
     <>
       {project && (
-        <div className="h-screen pt-20 text-center">
-          <h2>{project.title}</h2>
-          <p>회사: {project.company}</p>
-          <p>기술 스택: {project.skill.join(", ")}</p>
-          <div>프로젝트페이지</div>
-        </div>
+        <ScrollContainer>
+          <ScrollPage>
+            <Animator
+              animation={batch(Fade(), Sticky(), MoveOut(0, -500))}
+              className="flex flex-col items-center justify-center h-screen text-center"
+            >
+              <span className="text-4xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 break-keep">
+                {project.title}
+              </span>
+              <p className="mt-2 mb-5 text-xl font-bold text-gray-400">
+                {project.company}
+              </p>
+              <p className="max-w-lg mx-auto mb-10 text-lg font-medium leading-8 text-gray-700 break-keep">
+                {project.description}
+              </p>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage>
+            <Animator
+              animation={batch(Fade(), Sticky(), Zoom())}
+              className=" flex items-center h-screen text-center w-full max-w-[1440px] px-10 mx-auto mt-10 relative"
+            >
+              <div className="absolute left-0 flex items-center justify-center w-full h-10 top-10 ">
+                {imageLength.map((a, i) => (
+                  <ViewButton
+                    chk={a.chk}
+                    key={a.id}
+                    handleChkChange={(id: number, value: boolean) =>
+                      handleChkChange(a.id, value)
+                    }
+                  />
+                ))}
+              </div>
+              <div className="grid w-full grid-cols-2 grid-rows-2 gap-5 h-[800px]">
+                <div className="row-span-2 bg-[#FAFAFA] flex justify-center items-center relative">
+                  {imageLength.map((a, i) => (
+                    <img
+                      src={".././images/nineb/pad" + (i + 1) + ".png"}
+                      className={
+                        `${a.chk === true ? "opacity-100" : "opacity-0"}` +
+                        " w-[420px] opa absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transition-all duration-500"
+                      }
+                    ></img>
+                  ))}
+                  <img
+                    src=".././images/project/ipad.png"
+                    className="rotate-90 "
+                  ></img>
+                </div>
+                <div className=" bg-[#FAFAFA] flex justify-center items-center relative">
+                  {imageLength.map((a, i) => (
+                    <img
+                      src={".././images/nineb/m" + (i + 1) + ".png"}
+                      className={
+                        `${a.chk === true ? "opacity-100" : "opacity-0"}` +
+                        " w-[360px] opa absolute -translate-x-1/2 top-[45px] left-1/2 transition-all duration-500"
+                      }
+                    ></img>
+                  ))}
+                  <img
+                    src=".././images/project/imac.png"
+                    className="relative rotate-0"
+                  ></img>
+                </div>
+                <div className=" bg-[#FAFAFA] flex justify-center items-center relative z-10">
+                  {imageLength.map((a, i) => (
+                    <img
+                      src={".././images/nineb/phone" + (i + 1) + ".png"}
+                      className={
+                        `${a.chk === true ? "opacity-100" : "opacity-0"}` +
+                        " w-[140px] opa absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transition-all duration-500"
+                      }
+                    ></img>
+                  ))}
+                  <img
+                    src=".././images/project/iphone.png"
+                    className="rotate-0 max-h-80"
+                  ></img>
+                </div>
+              </div>
+            </Animator>
+          </ScrollPage>
+        </ScrollContainer>
       )}
     </>
+  );
+}
+
+interface ViewButtonProps {
+  key: number;
+  chk: boolean;
+  handleChkChange: (id: number, value: boolean) => void;
+}
+
+function ViewButton(props: ViewButtonProps) {
+  let [chk, setChk] = useState(props.chk);
+
+  useEffect(() => {
+    setChk(props.chk);
+  }, [props.chk]);
+
+  return (
+    <span
+      className={
+        `${props.chk === true ? "bg-yellow-500" : "bg-gray-300"}` +
+        " block w-8 h-8 mx-2 transition-all  rounded-full cursor-pointer hover:bg-yellow-300"
+      }
+      onClick={() => {
+        if (props.chk === false) {
+          setChk(true);
+          props.handleChkChange(props.key, !chk);
+        }
+      }}
+    ></span>
   );
 }
 
--- client/src/data/data.tsx
+++ client/src/data/data.tsx
@@ -5,6 +5,8 @@ const projectData = [
     company: "한결",
     skill: ["HTML", "Tailwind css", "React"],
     image: "/images/portfolio/m1.png",
+    description:
+      "React를 비롯한 여러 기술들을 활용하여 농업 네비게이션의 Front-end를 개발하였습니다. 공공 API를 이용해 데이터를 수집하고, react-chartjs-2를 활용하여 시각화를 구현했습니다. 또한 react query를 이용하여 데이터의 캐싱 및 관리를 효율적으로 처리했습니다. 이를 통해 사용자들이 더욱 편리하고 직관적으로 정보를 확인할 수 있도록 구현했습니다.",
   },
   {
     id: 1,
@@ -12,6 +14,8 @@ const projectData = [
     company: "팜커넥트",
     skill: ["HTML", "Tailwind css", "React", "React query", "공공 API"],
     image: "/images/farmconnect/m1.png",
+    description:
+      "React를 비롯한 여러 기술들을 활용하여 농업 네비게이션의 Front-end를 개발하였습니다. 공공 API를 이용해 데이터를 수집하고, react-chartjs-2를 활용하여 시각화를 구현했습니다. 또한 react query를 이용하여 데이터의 캐싱 및 관리를 효율적으로 처리했습니다. 이를 통해 사용자들이 더욱 편리하고 직관적으로 정보를 확인할 수 있도록 구현했습니다.",
   },
   {
     id: 2,
@@ -33,6 +37,9 @@ const projectData = [
     company: "나인비",
     skill: ["HTML", "CSS", "Javascript", "PHP", "Mysql"],
     image: "/images/nineb/m1.png",
+    description:
+      "HTML, CSS, Javascript, PHP, Mysql 등 다양한 기술을 활용하여 반응형 홈페이지를 개발했습니다. HTML과 CSS를 이용하여 디자인적인 부분을 구현하고, Javascript를 활용하여 동적인 요소를 추가했습니다. PHP와 Mysql을 이용하여 데이터베이스와 연동하여 동적인 콘텐츠를 구현했습니다. 이를 통해 사용자들이 모바일과 데스크탑 등 다양한 환경에서 웹사이트를 쉽게 이용할 수 있도록 구현했습니다.",
+    imageLength: 6,
   },
 ];
 
