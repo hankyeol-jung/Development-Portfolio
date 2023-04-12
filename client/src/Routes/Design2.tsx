1785664c8810ccbc6adc7dbe5d43a044c4cf587e
gksruf <gksruf0@cafe24.com>
2023-04-12 17:26:39 +0900

    페키지 삭제

--- client/src/Routes/Design2.tsx
+++ client/src/Routes/Design2.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState, ReactNode } from "react";
-import { Controller, Scene } from "react-scrollmagic";
-import { Tween, Timeline } from "react-gsap";
+import React, { useEffect, useState } from "react";
 import { projectData } from ".././data/data";
 
 interface Design1Props {
@@ -30,99 +28,19 @@ function Design2(props: Design1Props) {
   return (
     <>
       {project && (
-        <Controller>
-          <Scene triggerHook="onLeave" duration={1000} pin>
-            {(progress: number) => (
-              <div className="flex flex-col items-center justify-center h-screen text-center">
-                <Timeline totalProgress={progress} paused>
-                  <Tween
-                    from={{ y: "0%", opacity: "1" }}
-                    to={{ y: "-150%", opacity: "0" }}
-                  >
-                    <span className="text-4xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 break-keep">
-                      {project.title}
-                    </span>
-                    <p className="mt-2 mb-5 text-xl font-bold text-gray-400">
-                      {project.company}
-                    </p>
-                  </Tween>
-                  <Tween
-                    from={{ y: "0%", opacity: "1" }}
-                    to={{ y: "-100%", opacity: "0" }}
-                  >
-                    <p className="max-w-lg mx-auto mb-10 text-lg font-medium leading-8 text-gray-700 break-keep">
-                      {project.description}
-                    </p>
-                  </Tween>
-                </Timeline>
-              </div>
-            )}
-          </Scene>
-          <Scene triggerHook="onLeave" duration={1000} pin reverse={true}>
-            {(progress: number) => (
-              <div className="flex flex-col items-center justify-center h-screen text-center">
-                <Timeline
-                  totalProgress={progress}
-                  paused
-                  target={
-                    <div className="opacity-0 translate-y-[100%]">
-                      <span className="text-4xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 break-keep">
-                        {project.title}
-                      </span>
-                      <p className="mt-2 mb-5 text-xl font-bold text-gray-400 ">
-                        {project.company}
-                      </p>
-                    </div>
-                  }
-                >
-                  <Tween to={{ y: "0%", opacity: 1 }} position={0}></Tween>
-                  <Tween to={{ y: "-100%", opacity: 0 }} position={2}></Tween>
-                </Timeline>
-                <Timeline
-                  totalProgress={progress}
-                  paused
-                  target={
-                    <div className="opacity-0 translate-y-[50%]">
-                      <p className="max-w-lg mx-auto mb-10 text-lg font-medium leading-8 text-gray-700 break-keep">
-                        {project.description}
-                      </p>
-                    </div>
-                  }
-                >
-                  <Tween to={{ y: "0%", opacity: 1 }} position={1}></Tween>
-                  <Tween to={{ y: "-50%", opacity: 0 }} position={3}></Tween>
-                </Timeline>
-              </div>
-            )}
-          </Scene>
-          <Scene triggerHook="onLeave" duration={1000} pin>
-            {(progress: number) => (
-              <div className="flex flex-col items-center justify-center h-screen text-center">
-                <Timeline totalProgress={progress} paused>
-                  <Tween
-                    from={{ y: "150%", opacity: "0" }}
-                    to={{ y: "0%", opacity: "1" }}
-                  >
-                    <span className="text-4xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 break-keep">
-                      {project.title}
-                    </span>
-                    <p className="mt-2 mb-5 text-xl font-bold text-gray-400">
-                      {project.company}
-                    </p>
-                  </Tween>
-                  <Tween
-                    from={{ y: "50%", opacity: "0" }}
-                    to={{ y: "0%", opacity: "1" }}
-                  >
-                    <p className="max-w-lg mx-auto mb-10 text-lg font-medium leading-8 text-gray-700 break-keep">
-                      {project.description}
-                    </p>
-                  </Tween>
-                </Timeline>
-              </div>
-            )}
-          </Scene>
-        </Controller>
+        <>
+          <div className="flex flex-col items-center justify-center h-screen text-center">
+            <span className="text-4xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 break-keep">
+              {project.title}
+            </span>
+            <p className="mt-2 mb-5 text-xl font-bold text-gray-400">
+              {project.company}
+            </p>
+            <p className="max-w-lg mx-auto mb-10 text-lg font-medium leading-8 text-gray-100 break-keep">
+              {project.description}
+            </p>
+          </div>
+        </>
       )}
     </>
   );
